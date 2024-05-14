9e488456d7d71e837aa0debf4773e57fd8e0b152
Krittika2003 <ckrittika0603@gmail.com>
2024-05-14 12:12:26 +0530

    push krne do yaar

--- src/main/java/com/budget/chatGpt.java
+++ src/main/java/com/budget/chatGpt.java
@@ -75,7 +75,7 @@ public class chatGpt extends HttpServlet {
 		}
 
 		String url = "https://api.openai.com/v1/chat/completions";
-		String apiKey = "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";
+		String apiKey ="";
 
 		String model = "gpt-3.5-turbo";
 
