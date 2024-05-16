a4073d6e60472f978648e12c49dbba7aeaed5e58
Omra Akhtar <79746003+omraakhtarit2020@users.noreply.github.com>
2024-05-16 23:17:40 +0530

    Update chatGpt.java

--- src/main/java/com/budget/chatGpt.java
+++ src/main/java/com/budget/chatGpt.java
@@ -70,9 +70,9 @@ public class chatGpt extends HttpServlet {
 		}
 
 		String url = "https://api.openai.com/v1/chat/completions";
-		String apiKey = "";
+		String apiKey = "sk-proj-ZhENlsznjCR18YkZlkWJT3BlbkFJwUIHT4cHyNeXpwhE7GUG";
 
-		String model = "";
+		String model = "gpt-3.5-turbo";
 
 		try {
 			URL obj = new URL(url);
